af1f03c400782cca88a836128a02a766e82af81c
DevNitro5 <anishchhetri196@gmail.com>
2023-02-25 11:50:22 +0000

    change title

--- src/app/blog/head.tsx
+++ src/app/blog/head.tsx
@@ -1,9 +1,9 @@
-import { firstName } from '@/utils/config';
+import * as config from '@/utils/config';
 
 export default function Head() {
   return (
     <>
-      <title>{firstName} | Blogs</title>
+      <title>{config.firstName} | Blogs</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Get in touch with me" />
       <link rel="icon" href="/A_Black.png" />
