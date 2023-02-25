9bed784623d806b73a5556184f8da82c14827618
DevNitro5 <98446102+DevNitro5@users.noreply.github.com>
2023-02-25 08:35:42 +0545

    Update page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -7,8 +7,8 @@ export default function Contact() {
   return (
     <motion.div initial="hidden" animate="visible">
       <Navbar />
-      <div className="px-5 lg:px-12">
-        <h1 className="text-4xl text-red font-semibold text-center">Blogs</h1>
+      <div className="h-[85vh] grid place-content-center">
+        <h1 className="text-4xl text-red font-semibold text-center">Adding Soon</h1>
       </div>
     </motion.div>
   );
